ffc2cb65079b77dc0caa00ed042f985b0ae72f8e
hariando7 <rianaja2200022@gmail.com>
2024-10-05 09:22:47 +0700

    feat: card mobile dashboard admin utama

--- src/components/mobile_all_cards/mobileDivisionVerificationAdminApplicationHistoryCard.tsx
+++ src/components/mobile_all_cards/mobileDivisionVerificationAdminApplicationHistoryCard.tsx
@@ -1,13 +1,201 @@
 "use client"
 
+
 import React from 'react'
 
-function MobileDivisionVerificationAdminApplicationHistoryCard() {
+function mobileDivisionVerificationAdminApplicationHistoryCard() {
     return (
-        <div>
-            Ini Card Mobile dashboard
-        </div>
+        <>
+            <section className="w-full bg-line-10 rounded-lg shadow-md flex flex-col gap-y-4 p-7 mb-4">
+                <div className="w-full grid grid-cols-3">
+                    <div className="w-full text-[14px] md:text-[16px]">No.</div>
+                    <div className="w-full col-span-2 text-[14px] md:text-[16px]">
+                        : 1
+                    </div>
+                </div>
+
+                <div className="w-full grid grid-cols-3">
+                    <div className="w-full text-[14px] md:text-[16px]">
+                        Nama
+                    </div>
+                    <div className="w-full col-span-2 text-[14px] md:text-[16px]">
+                        : Irsyad Al-Haq Husein
+                    </div>
+                </div>
+
+                <div className="w-full grid grid-cols-3">
+                    <div className="w-full text-[14px] md:text-[16px]">
+                        NIP
+                    </div>
+                    <div className="w-full col-span-2 text-[14px] md:text-[16px]">
+                        : 1234567812345678
+                    </div>
+                </div>
+
+                <div className="w-full grid grid-cols-3">
+                    <div className="w-full text-[14px] md:text-[16px]">
+                        Layanan
+                    </div>
+                    <div className="w-full col-span-2 text-[14px] md:text-[16px]">
+                        : Pengajuan Pangkat
+                    </div>
+                </div>
+
+                <div className="w-full grid grid-cols-3">
+                    <div className="w-full text-[14px] md:text-[16px]">
+                        Tanggal
+                    </div>
+                    <div className="w-full col-span-2 text-[14px] md:text-[16px]">
+                        : 22 Maret 2024
+                    </div>
+                </div>
+
+                <div className="w-full grid grid-cols-3">
+                    <div className="w-full text-[14px] md:text-[16px]">
+                        Jam
+                    </div>
+                    <div className="w-full col-span-2 text-[14px] md:text-[16px]">
+                        : 22.00 WIB
+                    </div>
+                </div>
+
+                <div className="w-full grid grid-cols-3">
+                    <div className="w-full text-[14px] md:text-[16px]">
+                        Status
+                    </div>
+                    <div className="w-full col-span-2 text-[14px] md:text-[16px]">
+                        :  <span className='text-[#188B09] bg-[#188B09] bg-opacity-20 py-3 px-3 rounded-lg'>Divalidasi</span>
+                    </div>
+                </div>
+            </section>
+
+            <section className="w-full bg-line-10 rounded-lg shadow-md flex flex-col gap-y-4 p-7 mb-4">
+                <div className="w-full grid grid-cols-3">
+                    <div className="w-full text-[14px] md:text-[16px]">No.</div>
+                    <div className="w-full col-span-2 text-[14px] md:text-[16px]">
+                        : 2
+                    </div>
+                </div>
+
+                <div className="w-full grid grid-cols-3">
+                    <div className="w-full text-[14px] md:text-[16px]">
+                        Nama
+                    </div>
+                    <div className="w-full col-span-2 text-[14px] md:text-[16px]">
+                        : Irsyad Al-Haq Husein
+                    </div>
+                </div>
+
+                <div className="w-full grid grid-cols-3">
+                    <div className="w-full text-[14px] md:text-[16px]">
+                        NIP
+                    </div>
+                    <div className="w-full col-span-2 text-[14px] md:text-[16px]">
+                        : 1234567812345678
+                    </div>
+                </div>
+
+                <div className="w-full grid grid-cols-3">
+                    <div className="w-full text-[14px] md:text-[16px]">
+                        Layanan
+                    </div>
+                    <div className="w-full col-span-2 text-[14px] md:text-[16px]">
+                        : Pengajuan Pangkat
+                    </div>
+                </div>
+
+                <div className="w-full grid grid-cols-3">
+                    <div className="w-full text-[14px] md:text-[16px]">
+                        Tanggal
+                    </div>
+                    <div className="w-full col-span-2 text-[14px] md:text-[16px]">
+                        : 22 Maret 2024
+                    </div>
+                </div>
+
+                <div className="w-full grid grid-cols-3">
+                    <div className="w-full text-[14px] md:text-[16px]">
+                        Jam
+                    </div>
+                    <div className="w-full col-span-2 text-[14px] md:text-[16px]">
+                        : 22.00 WIB
+                    </div>
+                </div>
+
+                <div className="w-full grid grid-cols-3">
+                    <div className="w-full text-[14px] md:text-[16px]">
+                        Status
+                    </div>
+                    <div className="w-full col-span-2 text-[14px] md:text-[16px]">
+                        :  <span className='text-[#188B09] bg-[#188B09] bg-opacity-20 py-3 px-3 rounded-lg'>Divalidasi</span>
+                    </div>
+                </div>
+            </section>
+
+            <section className="w-full bg-line-10 rounded-lg shadow-md flex flex-col gap-y-4 p-7">
+                <div className="w-full grid grid-cols-3">
+                    <div className="w-full text-[14px] md:text-[16px]">No.</div>
+                    <div className="w-full col-span-2 text-[14px] md:text-[16px]">
+                        : 3
+                    </div>
+                </div>
+
+                <div className="w-full grid grid-cols-3">
+                    <div className="w-full text-[14px] md:text-[16px]">
+                        Nama
+                    </div>
+                    <div className="w-full col-span-2 text-[14px] md:text-[16px]">
+                        : Irsyad Al-Haq Husein
+                    </div>
+                </div>
+
+                <div className="w-full grid grid-cols-3">
+                    <div className="w-full text-[14px] md:text-[16px]">
+                        NIP
+                    </div>
+                    <div className="w-full col-span-2 text-[14px] md:text-[16px]">
+                        : 1234567812345678
+                    </div>
+                </div>
+
+                <div className="w-full grid grid-cols-3">
+                    <div className="w-full text-[14px] md:text-[16px]">
+                        Layanan
+                    </div>
+                    <div className="w-full col-span-2 text-[14px] md:text-[16px]">
+                        : Pengajuan Pangkat
+                    </div>
+                </div>
+
+                <div className="w-full grid grid-cols-3">
+                    <div className="w-full text-[14px] md:text-[16px]">
+                        Tanggal
+                    </div>
+                    <div className="w-full col-span-2 text-[14px] md:text-[16px]">
+                        : 22 Maret 2024
+                    </div>
+                </div>
+
+                <div className="w-full grid grid-cols-3">
+                    <div className="w-full text-[14px] md:text-[16px]">
+                        Jam
+                    </div>
+                    <div className="w-full col-span-2 text-[14px] md:text-[16px]">
+                        : 22.00 WIB
+                    </div>
+                </div>
+
+                <div className="w-full grid grid-cols-3">
+                    <div className="w-full text-[14px] md:text-[16px]">
+                        Status
+                    </div>
+                    <div className="w-full col-span-2 text-[14px] md:text-[16px]">
+                        :  <span className='text-[#188B09] bg-[#188B09] bg-opacity-20 py-3 px-3 rounded-lg'>Divalidasi</span>
+                    </div>
+                </div>
+            </section>
+        </>
     )
 }
 
-export default MobileDivisionVerificationAdminApplicationHistoryCard
+export default mobileDivisionVerificationAdminApplicationHistoryCard
